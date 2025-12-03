0a806554dcc50b922690bd4f5aec16dad71e296c
Eduardo Fernandes de Magalhaes Pimenta <ed83isme@gmail.com>
2025-12-03 03:41:57 +0000

    part2 phonebook step 1. Added main functionality that allows user to add a new name to the phonebook list

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,41 +1,5 @@
-// 2.4: Course information step 9
-
-const Header = ({ name }) => <h1>{name}</h1>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-);
-
-const Course = ({ course }) => (
-  <div>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
-);
-
-const Courses = ({ courses }) => (
-  <div>
-    {courses.map((course) => (
-      <Course key={course.id} course={course} />
-    ))}
-  </div>
-);
-
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p>Total number of exercises: {totalExercises}</p>;
-};
+// 2.5: Separate module step 10
+import Courses from "./components/Courses";
 
 const App = () => {
   const courses = [
---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+//2.6: The Phonebook Step 1
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName(""); // Clear the input field after adding
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
